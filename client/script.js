4ffdc7695ad09065315ffb688910a064658f68ae
Miguel Rôlo <manrrolo@gmail.com>
2023-12-14 16:57:24 +0000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,10 +1,9 @@
 // Adicione esta parte ao seu script.js
 window.onscroll = function () {
-    // Verifica a posição do scroll
-    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-      document.getElementById("navbar").classList.add("shrink");
-    } else {
-      document.getElementById("navbar").classList.remove("shrink");
-    }
-  };
-  
+  // Verifica a posição do scroll
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").classList.add("shrink");
+  } else {
+    document.getElementById("navbar").classList.remove("shrink");
+  }
+};

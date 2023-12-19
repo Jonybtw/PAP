258942445a89cb65d4a9ac151484da98e5c21070
Jonybtw <cego04643@gmail.com>
2023-12-19 17:57:31 +0000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -3,4 +3,14 @@ function toggleSidebar() {
   sidebar.style.width = sidebar.style.width === '250px' ? '0' : '250px';
 }
 
+/*
+let params = new URLSearchParams();
+params.append('username', vardoUser);
+params.append('password', password);
 
+let result = await axios.post('/login', params, {})
+if (!result) ..
+else {
+  login -> redirect
+}
+*/

828cb42b35864ec6dc3f7da2729eebbeb871e515
Jonybtw <cego04643@gmail.com>
2024-04-26 20:58:15 +0100

    Tidy up the code

--- new/server/scripts/utils/validate_token.mjs
+++ new/server/scripts/utils/validate_token.mjs
@@ -4,18 +4,18 @@ import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const token = req.query.token; // Retrieve token from query parameter
+  const token = req.query.token;
 
   if (!token) {
-    res.clearCookie('token'); // Clear the token cookie
-    return res.sendStatus(401); // Unauthorized
+    res.clearCookie('token');
+    return res.sendStatus(401);
   }
 
   jwt.verify(token, process.env.SECRET_TOKEN_KEY, (err, user) => {
     if (err) {
-      console.error('Error verifying token:', err); // Log any errors
-      res.clearCookie('token'); // Clear the token cookie
-      return res.sendStatus(403); // Forbidden
+      console.error('Error verifying token:', err);
+      res.clearCookie('token');
+      return res.sendStatus(403);
     }
     res.sendStatus(200); // OK
   });
